fc2c6a7d88a9a3752d6bfaa0501658362d41f9ce
N1rv2na <n1rv2na@outlook.com>
2026-02-27 14:28:30 +0800

    Lab pgtbl: implememt speed up system calls

--- kernel/proc.c
+++ kernel/proc.c
@@ -120,6 +120,14 @@ found:
   p->pid = allocpid();
   p->state = USED;
 
+  // Allocate a page to store struct usyscall
+  if ((p->pusyscall == (struct usyscall *)kalloc()) == 0) {
+    freeproc(p);
+    release(&p->lock);
+    return 0;
+  }
+  p->pusyscall->pid = p->pid;
+
   // Allocate a trapframe page.
   if((p->trapframe = (struct trapframe *)kalloc()) == 0){
     freeproc(p);
@@ -153,6 +161,10 @@ freeproc(struct proc *p)
   if(p->trapframe)
     kfree((void*)p->trapframe);
   p->trapframe = 0;
+  // free memory of struct usyscall
+  if(p->pusyscall)
+    kfree((void*)p->pusyscall);
+  p->pusyscall = 0;
   if(p->pagetable)
     proc_freepagetable(p->pagetable, p->sz);
   p->pagetable = 0;
@@ -196,6 +208,16 @@ proc_pagetable(struct proc *p)
     return 0;
   }
 
+  // map the struct usyscall's physical memory address pusyscall at virtual address USYSCALL
+  if(mappages(pagetable, USYSCALL, PGSIZE,
+              (uint64)(p->pusyscall), PTE_R | PTE_U) < 0){
+    uvmunmap(pagetable, TRAMPOLINE, 1, 0);
+    uvmunmap(pagetable, TRAPFRAME, 1, 0);
+    uvmunmap(pagetable, USYSCALL, 1, 0);
+    uvmfree(pagetable, 0);
+    return 0;
+  }
+
   return pagetable;
 }
 
@@ -206,6 +228,8 @@ proc_freepagetable(pagetable_t pagetable, uint64 sz)
 {
   uvmunmap(pagetable, TRAMPOLINE, 1, 0);
   uvmunmap(pagetable, TRAPFRAME, 1, 0);
+  // unmap page USYSCALL
+  uvmunmap(pagetable, USYSCALL, 1, 0);
   uvmfree(pagetable, sz);
 }
 
